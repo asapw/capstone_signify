8f1e6c0abf14113ee0a2c536d71cc481bfab356b
asapw <septianwijaya01@gmail.com>
2024-11-29 16:20:41 +0700

    merge sama camera tapi camera ui msh error

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,6 +17,9 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        ndk {
+            abiFilters.addAll(listOf("armeabi-v7a", "arm64-v8a"))
+        }
     }
 
     buildTypes {
@@ -40,8 +43,6 @@ android {
         viewBinding = true
         mlModelBinding = true
     }
-
-
 }
 
 dependencies {
