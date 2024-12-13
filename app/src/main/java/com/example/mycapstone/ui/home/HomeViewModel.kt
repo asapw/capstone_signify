9819c95b292a7edf8282b83be9b5be49e4a60a1a
KangBubur1 <95082947+KangBubur1@users.noreply.github.com>
2024-12-13 11:23:15 +0700

    revisions

--- app/src/main/java/com/example/mycapstone/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/mycapstone/ui/home/HomeViewModel.kt
@@ -108,7 +108,7 @@ class HomeViewModel : ViewModel() {
         viewModelScope.launch {
             _isLoading.value = true
             try {
-                val response = NewsConfig.getModelService().getNews("Sign Language", BuildConfig.NEWS_API_KEY)
+                val response = NewsConfig.getModelService().getNews("Sign Language", "61d5aab3aa6f4ccdb54d430263099499")
                 _news.value = response.articles // Assuming your API returns articles
                 _isLoading.value = false
             } catch (e: Exception) {
